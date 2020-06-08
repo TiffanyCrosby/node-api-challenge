ffa4069317dc0674c004fc74498ec1312e1fe0bc
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-06-07 21:12:54 -0400

    forgot middleware for server.js

--- server.js
+++ server.js
@@ -21,5 +21,17 @@ server.use('/api/actions', actionRouter);
 
 server.use(notFound);
 
+function logger(req, res, next){
+    const method = req.method;
+    const endpoint = req.originalUrl;
+    
+    console.log(`${method} to ${endpoint} ${base} at ${new Date().toISOString()}`);
+    next();
+}
+
+function notFound(req, res, next){
+    res.status(404).json({errorMessage: "Oops, we didn't find what you're looking for!"})
+}
+
 module.exports = server;
 
