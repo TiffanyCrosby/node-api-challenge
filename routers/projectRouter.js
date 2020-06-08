26951022fcdecb2cb5302511406f915264139a2a
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-06-07 22:59:33 -0400

    project router working with mw

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
     })
 });
 
-router.get('/:id',  (req, res) => {
+router.get('/:id', validateProjectId, (req, res) => {
     Projects.get(req.params.id)
     .then(project => {
       if(!project){
@@ -29,7 +29,7 @@ router.get('/:id',  (req, res) => {
     })
   });
 
-  router.get('/:id/actions',  (req, res) => {
+  router.get('/:id/actions', validateProjectId, (req, res) => {
     Projects.getProjectActions(req.params.id)
     .then(action => {
       if(action === 0){
@@ -50,18 +50,61 @@ router.get('/:id',  (req, res) => {
       res.status(500).json({ errorMessage: "There was an issue with saving user to the database."})
     })
   });
+
+  router.put('/:id', validateProjectId, validateProject, (req, res) => {
+	Projects.update(req.params.id, req.body)
+		.then((project) => {
+				res.status(200).json(project);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'The project information could not be updated.' });
+		});
+});
+
+  router.delete('/:id', validateProjectId, (req, res) => {
+    Projects.remove(req.params.id)
+    .then(project => {
+      if(project > 0){
+        res.status(200).json(`Successfully removed project!`)
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "The project could not be removed." });
+    })
+  });
+
   
 
-//   function validateProjectId(req, res, next) {
-//         Projects.get(req.params.id)
-//         .then(project => {
-//             if (!user){
-//                 res.status(400).json({ errorMessage: "Invalid user ID" })
-//             } else {
-//                 req.project = project;
-//                 next();
-//         }
-//         })
-//   }
+  function validateProjectId(req, res, next) {
+	Projects.get(req.params.id).then((projects) => {
+		if (!projects) {
+			res.status(404).json({ errorMessage: `Invalid Project ID.` });
+		} else {
+			req.projects = req.params.id;
+			next();
+		}
+	});
+}
 
+function validateProject(req, res, next) {
+	if (Object.keys(req.body).length === 0) {
+		res.status(400).json({ message: 'Missing Data.' });
+	} else if (!req.body.name || !req.body.description) {
+		res.status(400).json({ message: 'Name and Description required' });
+	} else {
+		return next();
+	}
+}
+
+    // function validateAction(req, res, next) {
+    //     if (Object.keys(req.body).length === 0) {
+    //         res.status(400).json({ message: 'Missing Data.' });
+    //     } else if (!req.body.description || !req.body.notes) {
+    //         res.status(400).json({ message: 'Description and Notes required.' });
+    //     } else {
+    //         return next();
+    //     }
+    // }
 module.exports = router;
